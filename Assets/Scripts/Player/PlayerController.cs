6bee3449f879e854bd2e89bec3c750a8b3be18e5
marcin.gaik <marcin.gaik@tensquaregames.com>
2021-11-22 17:06:23 +0100

    some attributes fixes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,28 +6,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform rootTransform;
     [SerializeField] private Rigidbody rigidbody;
 
-    private bool isInputForward;
-
-    private float targetForwardSpeed;
-    private float currentForwardSpeed;
-
     private void Awake()
     {
-        rigidbody.maxAngularVelocity = 1;
+        rigidbody.maxAngularVelocity = GameManager.Instance.playerSettings.selectedVehicle.rotationMaxRadiansPerSec;
     }
 
     public void Rotate(Vector3 input)
     {
-        rigidbody.AddRelativeTorque(0, input.y * GameManager.Instance.playerSettings.selectedVehicle.rotateSpeed, 0,
+        rigidbody.AddRelativeTorque(0, input.y * GameManager.Instance.playerSettings.selectedVehicle.rotationAcceleration, 0,
             ForceMode.Impulse);
     }
 
     public void Move(Vector3 input)
     {
-        if (rigidbody.velocity.magnitude < GameManager.Instance.playerSettings.selectedVehicle.maxSpeed)
+        if (rigidbody.velocity.magnitude < GameManager.Instance.playerSettings.selectedVehicle.moveMaxSpeed)
         {
             Vector3 fowardSpeed =
-                new Vector3(0, 0, input.y * GameManager.Instance.playerSettings.selectedVehicle.acceleration);
+                new Vector3(0, 0, input.y * GameManager.Instance.playerSettings.selectedVehicle.moveAcceleration);
             rigidbody.AddRelativeForce(fowardSpeed, ForceMode.Impulse);
         }
     }
--- Assets/Scripts/Vehicle/VehicleAttribtes.cs
+++ Assets/Scripts/Vehicle/VehicleAttribtes.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public abstract class VehicleAttribtes : ScriptableObject
 {
-    [Range(1, 100)] public float acceleration = 10;
-    
-    [Range(1, 100)] public float maxSpeed = 10;
-    
-    [Range(1, 500)] public float rotateSpeed = 1;
+    [Range(1, 100)] public float moveMaxSpeed = 10;
+
+    [Range(1, 100)] public float moveAcceleration = 10;
+
+    public ForceMode moveForceMode = ForceMode.Impulse;
+
+    [Range(1, 500)] public float rotationAcceleration = 1;
+    public float rotationMaxRadiansPerSec = 2;
+    public ForceMode rotationForceMode = ForceMode.Impulse;
 
     [Range(1, 1000)] public float maxHealth = 100;
-
     [Range(1, 1000)] public float maxFuel = 500;
 }
